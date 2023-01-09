9950c7a64a2c19bcb79277c6c1bc973fec2ca15e
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-01-08 22:19:02 -0300

    ex01

---
+++ java_spring/src/application/ex1.java
@@ -0,0 +1,16 @@
+package application;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		
+		/* ler os dados de um trabalhador com N contratos (N fornecido pelo user)
+		 * Depois solicitar do user um mês e mostrar qual foi o sálario do funcionário 
+		 * nesse mês.
+		 * */
+		
+		
+
+	}
+
+}
